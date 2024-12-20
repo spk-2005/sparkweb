130a0cc507138d13384a672424829007ff335a21
spk-2005 <prasannasimha5002@gmail.com>
2024-12-20 07:34:57 +0530

    commit

--- src/home.js
+++ src/home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default function Home() {
+export default function Home() {  
+  
+  useEffect(() => {
+  const script = document.createElement('script');
+  script.src = 'https://vemtoutcheeg.com/400/8674727';
+  script.async = true;
+
+  try {
+    (document.body || document.documentElement).appendChild(script);
+  } catch (e) {
+    console.error('Error appending the script:', e);
+  }
+
+  return () => {
+    // Clean up the script if the component is unmounted
+    if (script.parentNode) {
+      script.parentNode.removeChild(script);
+    }
+  };
+}, []);
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home</h1><div id="ad-container">Your Ad will appear here</div>
     </div>
   )
 }
