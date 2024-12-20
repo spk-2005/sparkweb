f3934b267b5f442044290a5b71f98da6a1b15c8c
spk-2005 <prasannasimha5002@gmail.com>
2024-12-20 07:53:22 +0530

    commit

--- src/home.js
+++ src/home.js
@@ -1,25 +1,27 @@
 import React, { useEffect } from 'react'
 
 export default function Home() {  
-  
-  useEffect(() => {
+useEffect(() => {
   const script = document.createElement('script');
-  script.src = 'https://vemtoutcheeg.com/400/8674727';
+  script.src = 'https://alwingulla.com/88/tag.min.js'; // Script source URL
   script.async = true;
+  script.setAttribute('data-zone', '120573'); // Set the data-zone attribute
+  script.setAttribute('data-cfasync', 'false'); // Set the data-cfasync attribute
 
   try {
-    (document.body || document.documentElement).appendChild(script);
-  } catch (e) {
-    console.error('Error appending the script:', e);
+    document.body.appendChild(script); // Append the script to the DOM
+  } catch (error) {
+    console.error('Error appending the script:', error);
   }
 
   return () => {
-    // Clean up the script if the component is unmounted
+    // Cleanup the script on component unmount
     if (script.parentNode) {
       script.parentNode.removeChild(script);
     }
   };
 }, []);
+
   return (
     <div>
       <h1>Home</h1><div id="ad-container">Your Ad will appear here</div>
